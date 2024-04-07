0eee2a30ddbbc4ea59a90fbb210ade935b857f62
frontend-tester02 <shokhrukhnuriddinov@gamil.com>
2024-04-08 00:49:20 +0900

    Add ech person price part

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const tipInputs = document.querySelectorAll('.input-value')
 // Split bill
 const tipPersonNum = document.querySelector('.tip-num-person');
 const billMinus = document.querySelector('.bill-btn-minus');
-const billPlus = document.querySelector('.bill-btn-plus')
+const billPlus = document.querySelector('.bill-btn-plus');
 
 
 calcInputValue.addEventListener('change', calculateTotal);
@@ -58,29 +58,48 @@ const calcTipBtn = document.querySelector('.calc-btn');
 calcTipBtn.addEventListener('click', calculateTotal)
 
 
+// calc total price
 const tipTotalPrice = document.querySelector('.tip-total-price');
 const totalPrice = document.querySelector('.total-price')
 let tipTotal;
 let total;
+
+
+// Calc for each person
+const tipEachContent = document.querySelector('.content')
+const tipEachPerson = document.querySelector('.tip-each-price');
+const tipEachTotal = document.querySelector('.total-each-price');
+
+let tipEach;
+let eachTotal;
 
 function calculateTotal() {
   let billAmount = Number(calcInputValue.value);
   let tipPercent = Number(tipPercentValue.value);
   let billPerson = Number(tipPersonNum.value);
 
-  tipTotal = parseFloat(billAmount * ((tipPercent / 100) / billPerson)).toFixed(2)
-  total = parseFloat((billAmount + parseFloat(tipTotal)) / billPerson).toFixed(2)
+  tipTotal = parseFloat(billAmount * ((tipPercent / 100))).toFixed(2);
+  total = parseFloat((billAmount + parseFloat(tipTotal))).toFixed(2);
 
   tipTotalPrice.textContent = `$ ${tipTotal}`;
   totalPrice.textContent = `$ ${total}`;
+
+  tipEach = parseFloat(tipTotal / billPerson).toFixed(2)
+  eachTotal = parseFloat(total / billPerson).toFixed(2)
+
+  tipEachPerson.textContent = `$${tipEach}`
+  tipEachTotal.textContent = `$${eachTotal}`
+
 }
 
 
 clearTipBtn.addEventListener('click', () => {
-  calcInputValue.value = '';
-  tipPercentValue.value = '';
-  tipPersonNum.value = ''
-  tipTotalPrice.textContent = '',
-  totalPrice.textContent = '';
+  calcInputValue.value = '0';
+  tipPercentValue.value = '0';
+  tipPersonNum.value = '0'
+  tipTotalPrice.textContent = '$ 0.00',
+  totalPrice.textContent = '$ 0.00';
+  tipEachPerson.textContent = '$ 0.00';
+  tipEachTotal.textContent = '$ 0.00'
 
 })
